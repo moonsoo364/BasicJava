650e45cdaf4dc1c56fe5dd791442b31dbda362d1
aptx15hibr@naver.com <aptx15hibr@naver.com>
2023-07-09 11:14:51 +0900

    init commit

---
+++ src/ch9/Ex9_5.java
@@ -0,0 +1,33 @@
+package ch9;
+
+ class Card2 {
+	String kind;
+	int number;
+	
+	Card2(){
+		this("SPADE",1);
+	}
+	
+	public Card2(String kind, int number) {
+		this.kind = kind;
+		this.number = number;
+	}
+
+	@Override
+	public String toString() {
+		return "Card2 [kind=" + kind + ", number=" + number + "]";
+	}
+	
+	
+
+}
+
+ public class Ex9_5{
+	public static void main(String[] args) {
+		Card2 c1 = new Card2();
+		Card2 c2 = new Card2("HEART", 10);
+		System.out.println(c1.toString());
+		System.out.println(c2.toString());
+		
+	}
+}
